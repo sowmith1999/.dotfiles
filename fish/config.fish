f10f076e8d6c9a5256f335b91e73384cd8880941
SowmithK <sowmithkunapaneni@gmail.com>
2024-03-25 12:03:58 -0500

    added nvim and personal as submodules and bunch of changes across fish, tmux and alacritty

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set --export PATH $PATH $HOME/.local/bin/scripts
+
+abbr -a xclip 'xclip -selection clipboard'
+abbr -a ls 'exa'
+abbr -a ll 'exa -lah' 
+abbr -a dotfiles 'nvim ~/.dotfiles'
